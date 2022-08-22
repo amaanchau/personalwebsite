094f37f71598f95ed30a8ff982175a5ad98e0f1e
Amaan Chaudhry <amaanchau7@gmail.com>
2022-08-21 21:24:11 -0500

    1. Created Home Page Elements and links 2.Added Email and Phone Call Links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import NavBar from "./NavBar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -11,18 +10,15 @@ import NotFoundPage from "./pages/NotFoundPage";
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* <NavBar /> */}
-        <div id="page-body">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/extracurriculars" element={<Extracurriculars />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </div>
+      <div id="page-body">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/extracurriculars" element={<Extracurriculars />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </div>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,52 @@
 import React from "react";
+import profpic from "../assets/profpic.jpg";
+import resume from "../assets/resume.pdf";
+import { Link } from "react-router-dom";
 
 const Home = () => (
   <>
-    <h1>Hello!</h1>
+    <ul>
+      <li>
+        <a href="mailto:amaanchau@tamu.edu?">Menu</a>
+      </li>
+      <li>
+        <a href="mailto:amaanchau@tamu.edu?">Contact</a>
+      </li>
+    </ul>
+    <h1>Howdy, Friends!</h1>
+    <p>My name is Amaan Chaudhry, and I'm a student at Texas A&M University.</p>
+    <p>
+      Email: <a href="mailto:amaanchau@tamu.edu?">amaanchau@tamu</a>
+    </p>
+    <p>
+      Phone: <a href="tel:2812355003">(281) 235-5003</a>
+    </p>
+    <img src={profpic} alt="Profile Picture" width="200" height="200" />
+    <h3>Quick Links:</h3>
+    <nav>
+      <ul>
+        <li>
+          <a href="www.linkedin.com/in/amaan-chaudhry">Linkedin</a>
+        </li>
+        <li>
+          <a href={resume}>Resume</a>
+        </li>
+        <li>
+          <a href="https://github.com/amaanchau">Github</a>
+        </li>
+      </ul>
+    </nav>
+
+    <nav>
+      <ul>
+        <li>
+          <Link to="/portfolio">View My Projects</Link>
+        </li>
+        <li>
+          <Link to="/about">Learn More About Me</Link>
+        </li>
+      </ul>
+    </nav>
   </>
 );
 
