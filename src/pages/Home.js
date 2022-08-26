27fb68e5cd01a1bc75aa1fafdc20e4746a797bbe
Amaan Chaudhry <amaanchau7@gmail.com>
2022-08-26 02:34:20 -0500

    1. Completed extra link buttons 2. Added on load animations 3. Adjust content based on sizing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,92 +26,90 @@ const Home = () => (
       <div className="left">
         <div className="Title">Howdy, World</div>
         <hr />
-        <span>My name is </span>
-        <span className="name">Amaan Chaudhry</span>
-        <span>, and I'm a student at </span>
-        <span className="tamu">Texas A&M University</span>
-        <span>.</span>
-        <br />
-        <span>Email: </span>
-        <span className="links">
-          <a className="contactlink" href="mailto:amaanchau@tamu.edu?">
-            amaanchau@tamu
-          </a>
-        </span>
-        <br />
-        <span>Phone: </span>
-        <span className="links">
-          <a className="contactlink" href="tel:2812355003">
-            (281) 235-5003
-          </a>
-        </span>
-        <br />
+        <div className="middleleftcontent">
+          <span className="text">My name is </span>
+          <span className="name">Amaan Chaudhry</span>
+          <span className="text">, and I'm a student at </span>
+          <span className="tamu">Texas A&M University</span>
+          <span className="text">.</span>
+          <br />
+          <span className="text">Email: </span>
+          <span className="links">
+            <a className="contactlink" href="mailto:amaanchau@tamu.edu?">
+              amaanchau@tamu
+            </a>
+          </span>
+          <br />
+          <span className="text">Phone: </span>
+          <span className="links">
+            <a className="contactlink" href="tel:2812355003">
+              (281) 235-5003
+            </a>
+          </span>
+          <br />
 
-        <p className="quicklinkheader">Quick Links:</p>
-        <nav>
-          <ul>
-            <li className="quicklinklist">
-              <a
-                target="_blank"
-                className="quicklinks"
-                href="https://www.linkedin.com/in/amaan-chaudhry"
-              >
-                <img
-                  className="quicklinkpics"
-                  src={linkedinlogo}
-                  alt="linkedin logo"
-                />
-                Linkedin
-              </a>
-            </li>
-            <li className="quicklinklist">
-              <a className="quicklinks" target="_blank" href={resume}>
-                <img
-                  className="quicklinkpics"
-                  src={resumelogo}
-                  alt="linkedin logo"
-                />
-                Resume
-              </a>
-            </li>
-            <li className="quicklinklist">
-              <a
-                target="_blank"
-                className="quicklinks"
-                href="https://github.com/amaanchau"
-              >
-                <img
-                  className="quicklinkpics"
-                  src={githublogo}
-                  alt="linkedin logo"
-                />
-                Github
-              </a>
-            </li>
-          </ul>
-        </nav>
+          <p className="quicklinkheader">Quick Links:</p>
+          <nav>
+            <ul>
+              <li className="quicklinklist">
+                <a
+                  target="_blank"
+                  className="quicklinks"
+                  href="https://www.linkedin.com/in/amaan-chaudhry"
+                >
+                  <img
+                    className="quicklinkpics"
+                    src={linkedinlogo}
+                    alt="linkedin logo"
+                  />
+                  Linkedin
+                </a>
+              </li>
+              <li className="quicklinklist">
+                <a className="quicklinks" target="_blank" href={resume}>
+                  <img
+                    className="quicklinkpics"
+                    src={resumelogo}
+                    alt="linkedin logo"
+                  />
+                  Resume
+                </a>
+              </li>
+              <li className="quicklinklist">
+                <a
+                  target="_blank"
+                  className="quicklinks"
+                  href="https://github.com/amaanchau"
+                >
+                  <img
+                    className="quicklinkpics"
+                    src={githublogo}
+                    alt="linkedin logo"
+                  />
+                  Github
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-
       <div className="right">
-        <img
-          className="profpic"
-          src={profpic}
-          alt="Profile Picture"
-          width="200"
-          height="200"
-        />
-
-        <nav>
-          <ul>
-            <li>
-              <Link to="/portfolio">View My Projects</Link>
-            </li>
-            <li>
-              <Link to="/about">Learn More About Me</Link>
-            </li>
-          </ul>
-        </nav>
+        <img className="profpic" src={profpic} alt="Profile Picture" />
       </div>
+      <nav className="bottom">
+        <ul>
+          <li className="morelist">
+            <Link className="more" to="/portfolio">
+              View My Projects
+            </Link>
+          </li>
+          <li className="morelist">
+            <Link className="more" to="/about">
+              Learn More About Me
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   </>
 );
